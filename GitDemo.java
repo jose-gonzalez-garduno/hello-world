759334208b9ef2de264eba99c7fc816b51bfeb49
Jose Gonzalez <joseag1999@gmail.com>
2021-02-24 18:55:23 -0700

    Update GitDemo.java
    For addNumber(), added the argument variable ‘num’ to ‘previousNumber’ variable.
    For reverseNumber(), added the statement "reverseNum = newNumber;".
    For GitDemo class, added “private double reverseNum;”.

--- GitDemo.java
+++ GitDemo.java
@@ -1,10 +1,10 @@
 /*****
- * Edit this line to have your name.
+ * Jose Gonzalez
  ******/
 public class GitDemo {
     
     private double previousNumber;
-    
+    private double reverseNum;
     public GitDemo() {
         previousNumber = 1;
     }
@@ -13,8 +13,8 @@ public class GitDemo {
      * This function should add 'num' to 'previousNumber' and return the sum.
     *****/
     public double addNumber(double num) {
-        System.out.print("Add num to previousNumber");
-        return 0;
+        double sum = num + previousNumber;
+        return sum;
     }
     
     /*****
@@ -45,7 +45,7 @@ public class GitDemo {
         for(int i = 1; i <= num; ++i){
             newNumber = newNumber * i;
         }
-        
+        reverseNum = newNumber;
         return newNumber;
     }
     
