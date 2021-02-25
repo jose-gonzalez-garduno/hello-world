be36628c7035a2d835b13e2ac39f4047ef0e2904
Jose Gonzalez <jagonz68@asu.edu>
2021-02-24 20:29:58 -0700

    Swap code fixed
    Swapped the code within findFactorial() and reverseNumber()

--- GitDemo.java
+++ GitDemo.java
@@ -18,11 +18,11 @@ public class GitDemo {
     }
     
     /*****
-     * This function should reverse 'num' and return the resulting value.
-     * i.e. if num = 1234
-     *  the function will return 4321
+     * This function should calculate the factorial of 'num' and return the resulting value.
+     * i.e. if num = 4,
+     *  the function will return the value of (1 * 2 * 3 * 4)
      *****/
-    public double reverseNumber(int num){
+    public double findFactorial(int num) {
         double newNumber = 1;
         
         for(int i = 1; i <= num; ++i){
@@ -32,12 +32,12 @@ public class GitDemo {
         return newNumber;
     }
     
-        /*****
-     * This function should calculate the factorial of 'num' and return the resulting value.
-     * i.e. if num = 4,
-     *  the function will return the value of (1 * 2 * 3 * 4)
+    /*****
+     * This function should reverse 'num' and return the resulting value.
+     * i.e. if num = 1234
+     *  the function will return 4321
      *****/
-    public double findFactorial(int num) {
+    public double reverseNumber(int num){
         double factorialNumber = 0;
         
         while( num != 0 ) {
@@ -46,6 +46,7 @@ public class GitDemo {
             num /= 10;
         }
         
-        return factorialNumber;
-    } 
+        return factorialNumber;        
+    }
+    
 }
